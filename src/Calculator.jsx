5299c0eeeb7696216489d3e90365e0f7d8b51d19
PC wonton <wontonking123@outlook.com>
2022-10-02 02:40:07 +0200

    removed react junk

---
+++ src/Calculator.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+const Calculator = () => {
+    return(
+        <h1>
+            <div>equal</div>
+            <div>+</div>
+            <div>-</div>
+            <div>backspace</div>
+            <div>%</div>
+            <div>3</div>
+            <div>6</div>
+            <div>9</div>
+            <div>x</div>
+            <div>0</div>
+            <div>2</div>
+            <div>5</div>
+            <div>8</div>
+            <div>divide</div>
+            <div>.</div>
+            <div>1</div>
+            <div>4</div>
+            <div>7</div>
+            <div>AC</div>
+        </h1>
+    )
+}
+
+export default Calculator()
